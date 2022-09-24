ecaa62eb7e9db9c4b008d8a054bd6fb80c046c4c
Abraham Maleko <abrahammaleko@gmail.com>
2022-09-24 17:57:57 +0300

    added migrations for tracking location

---
+++ database/migrations/2022_09_24_140532_add_track_location_address_to_short_urls_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('short_urls', function (Blueprint $table) {
+            //
+            $table->boolean('track_location')->after('track_ip_address');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('short_urls', function (Blueprint $table) {
+            //
+        });
+    }
+};
